aaf69d512c38db7425e3049bd1f62fedba0c330d
tnsweb <156802791+tnsweb@users.noreply.github.com>
2024-04-25 18:04:54 +0800

    Update script.js maint

--- not-available/script.js
+++ not-available/script.js
@@ -1,5 +1,5 @@
 let isUnderMaintanence = new Date() < new Date(2023, 5, 8, 7, 0, 0, 0);
 if ((isUnderMaintanence === true) && (location.hostname.split(".")[1] !== "id")) {
 	localStorage.setItem("expectedCompletionTime", "Thu 8 June 2023, 07:00:00");
-	location.replace("maintenance-mode.html");
+	location.replace("./");
 }
