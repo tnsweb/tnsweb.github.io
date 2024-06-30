a8aedf3e5bdcd6d2be5b9d3403048af48cc27430
tnsweb <156802791+tnsweb@users.noreply.github.com>
2024-06-30 12:02:36 +0800

    Update hw.js

--- hw.js
+++ hw.js
@@ -1,23 +1,10 @@
 let userObj = JSON.parse(localStorage.getItem("accounts-v2-userObj"));
+const isGuest = !!userObj.guest;
 
 document.getElementById("iconlink").href = "/logos/logo.png";
 document.title = "Homework - " + (userObj.GEPclass.name||userObj.mixedFC.name) + " 2024";
 
 let homework = [
-	{
-		classN: "6-3",
-		startdate: new Date(2024, 4, 10),
-		duedate: new Date(2024, 4, 13),
-		subject: "MATH",
-		title: "Filing and Corrections - Pie Charts, CT, Bring Maths File"
-	},
-	{
-		classN: "6-3",
-		startdate: new Date(2024, 4, 10),
-		duedate: new Date(2024, 4, 13),
-		subject: "MATH",
-		title: "SLS on Nets (due date not confirmed)"
-	}
 	/*Format:
  
  		classN: "class_name" e.g. "5-3", "5J"
@@ -29,43 +16,43 @@ let homework = [
 ]
 
 
+if (!isGuest) {
 
-
-let hwbody = document.getElementById("tbody")
-let now = new Date()
-let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
-let userClasses = [JSON.parse(localStorage.getItem("accounts-v2-userObj")).GEPclass.name, JSON.parse(localStorage.getItem("accounts-v2-userObj")).mixedFC.name]
-for (j=0;j<homework.length;j++) {
-	i = homework[j]
-
-	if (!userClasses.includes(i.classN)) continue
+	let hwbody = document.getElementById("tbody")
+	let now = new Date()
+	let days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+	let userClasses = [userObj.GEPclass.name, userObj.mixedFC.name]
+	for (j=0;j<homework.length;j++) {
+		i = homework[j]
 	
-	row = document.createElement("tr")
+		if (!userClasses.includes(i.classN)) continue
+		
+		row = document.createElement("tr")
+		
+		cell1 = document.createElement("td")
+		cell2 = document.createElement("td")
+		cell3 = document.createElement("td")
+		cell4 = document.createElement("td")
 	
-	cell1 = document.createElement("td")
-	cell2 = document.createElement("td")
-	cell3 = document.createElement("td")
-	cell4 = document.createElement("td")
-
-	cell1.innerText = String(i.startdate.getDate()).padStart(2, "0") + "/" + String(i.startdate.getMonth()+1).padStart(2, "0") + " " + days[i.startdate.getDay()]
-	cell2.innerText = i.subject
-	cell3.innerText = i.title
-	cell4.innerText = String(i.duedate.getDate()).padStart(2, "0") + "/" + String(i.duedate.getMonth()+1).padStart(2, "0") + " " + days[i.duedate.getDay()]
-
-	row.appendChild(cell1)
-	row.appendChild(cell2)
-	row.appendChild(cell3)
-	row.appendChild(cell4)
+		cell1.innerText = String(i.startdate.getDate()).padStart(2, "0") + "/" + String(i.startdate.getMonth()+1).padStart(2, "0") + " " + days[i.startdate.getDay()]
+		cell2.innerText = i.subject
+		cell3.innerText = i.title
+		cell4.innerText = String(i.duedate.getDate()).padStart(2, "0") + "/" + String(i.duedate.getMonth()+1).padStart(2, "0") + " " + days[i.duedate.getDay()]
 	
-	hwbody.appendChild(row)
-	startdate = i.startdate
-	startdate.setHours(12, 29)
-	duedate = i.duedate
-	duedate.setHours(12, 29)
-	if (startdate > now) {
-		row.classList.add("upcoming")
-	} else if (duedate < now) {
-		row.classList.add("overdue")
+		row.appendChild(cell1)
+		row.appendChild(cell2)
+		row.appendChild(cell3)
+		row.appendChild(cell4)
+		
+		hwbody.appendChild(row)
+		startdate = i.startdate
+		startdate.setHours(12, 29)
+		duedate = i.duedate
+		duedate.setHours(12, 29)
+		if (startdate > now) {
+			row.classList.add("upcoming")
+		} else if (duedate < now) {
+			row.classList.add("overdue")
+		}
 	}
 }
-
